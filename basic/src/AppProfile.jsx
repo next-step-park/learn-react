8cb59f8187499be3c8b931751ff26012e5adad84
next-step-park <next.step.park@gmail.com>
2022-12-08 10:34:40 +0900

    Avatar Component取り出し

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Avatar from './components/Avatar';
 import Profile from './components/Profile'
 
 function AppProfile() {
     return (
-        <>
+        <> 
+            <Avatar isNew={true} image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'/>
             {/* propsの値としてtrueを返したい場合、JSXを利用すれば良い */}
             <Profile image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80' name='SIYEON PARK' title='Frontend Developer'/>
             <Profile isNew={true} image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80' name='SEOYEON KIM' title='Manager'/>
