e25611f2c88310ca6622a8d25b760da2e9d79952
next-step-park <next.step.park@gmail.com>
2022-12-08 09:15:47 +0900

    basic JSX syntax

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'siyeon';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='orange'>hello</h1>
+      <h2>{name}</h2>
+      <ul>
+        <li>林檎</li>
+        <li>バナナ</li>
+        <li>牛乳</li>
+      </ul>
+      <img
+        // javascriptコードをObjectとして扱っているので、{}が2つである
+        style={{width: '200px', height: '200px'}} 
+        src='https://images.unsplash.com/photo-1670348283436-ca7f570483c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt='road' />
+    </>
   );
 }
 
