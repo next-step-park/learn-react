28347a4b1e0b5162f79b1436791303e813c9463d
next-step-park <next.step.park@gmail.com>
2022-12-08 11:08:35 +0900

    useStateを利用して、countを増加させるbutton作成

---
+++ basic/src/AppCount.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter';
+import './App.css'
+
+export default function AppCount() {
+    return (
+        <div className='container'>
+            <Counter />
+        </div>
+    );
+}
+
---
+++ basic/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+    const [number, setNumber] = useState(0);
+
+    return (
+        <div className='counter'>
+            <span className='number'>{number}</span>
+            <button className='button' onClick={(event) => {
+                setNumber(number + 1);
+            }}>Add +</button>
+        </div>
+    );
+}
+
