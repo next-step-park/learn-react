1e967b8864ed385cc253397b4e059c474574a408
next-step-park <next.step.park@gmail.com>
2022-12-08 10:08:20 +0900

    logical演算子を使って、trueの場合だけnew tagを描画する

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -4,8 +4,9 @@ import Profile from './components/Profile'
 function AppProfile() {
     return (
         <>
+            {/* propsの値としてtrueを返したい場合、JSXを利用すれば良い */}
             <Profile image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80' name='SIYEON PARK' title='Frontend Developer'/>
-            <Profile image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80' name='SEOYEON KIM' title='Manager'/>
+            <Profile isNew={true} image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80' name='SEOYEON KIM' title='Manager'/>
             <Profile image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80' name='JOONGKYU PARK' title='Backend Developer'/>
         </>
     );
