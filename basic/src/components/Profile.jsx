269c7227f0968b03c909bfc939a472d14283476a
next-step-park <next.step.park@gmail.com>
2022-12-08 09:43:00 +0900

    Profile Component作成

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function Profile() {
     return (
-        <div>
-            Profile
+        <div className='profile'>
+            <img
+                className='photo'
+                src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                alt="avatar" />
+                <h1>SIYEON PARK</h1>
+                <p>Frontend Developer</p>
         </div>
     );
 }
