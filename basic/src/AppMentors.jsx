e5c55d5f04226bcf929e92dda7bea328f9395928
next-step-park <next.step.park@gmail.com>
2022-12-09 22:25:14 +0900

    status add, update (refectoring)

--- basic/src/AppMentors.jsx
+++ basic/src/AppMentors.jsx
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from "react";
 
 const AppMentors = () => {
-    const [person, setPerson] = useState({
-        name: "siyeon",
-        title: "developer",
-        mentors: [
-            {
-                name: "john",
-                title: "senior developer",
-            },
-            {
-                name: "bob",
-                title: "senior developer",
-            },
-        ],
-    });
+    const [person, setPerson] = useState(initialPerson);
+    const handleAdd = () => {
+        const name = prompt(`追加したいエルダーの名前は何でしょうか？`);
+        const title = prompt(`追加したエルダーの職種は何でしょうか？`);
+
+        setPerson((person) => ({
+            ...person,
+            mentors: [...person.mentors, { name, title }],
+        }));
+    };
+    const handledelete = () => {
+        const name = prompt(`削除したいエルダーの名前は何でしょうか？`);
+
+        setPerson((person) => ({
+            ...person,
+            mentors: person.mentors.filter((m) => m.name !== name),
+        }));
+    };
+    const handleUpdate = () => {
+        const prev = prompt(`誰の名前を変えたいんですか？`);
+        const current = prompt(`名前を何にしますか？`);
+
+        setPerson((person) => ({
+            ...person,
+            mentors: person.mentors.map((mentor) => {
+                if (mentor.name === prev) {
+                    return { ...mentor, name: current };
+                }
+                return mentor;
+            }),
+        }));
+    };
 
     return (
         <div>
@@ -29,54 +47,26 @@ const AppMentors = () => {
                     </li>
                 ))}
             </ul>
-            <button
-                onClick={() => {
-                    const prev = prompt(`誰の名前を変えたいんですか？`);
-                    const current = prompt(`名前を何にしますか？`);
-
-                    setPerson((person) => ({
-                        ...person,
-                        mentors: person.mentors.map((mentor) => {
-                            if (mentor.name === prev) {
-                                return { ...mentor, name: current };
-                            }
-                            return mentor;
-                        }),
-                    }));
-                }}
-            >
-                エルダーの名前を変える
-            </button>
-            <button
-                onClick={() => {
-                    const name =
-                        prompt(`追加したいエルダーの名前は何でしょうか？`);
-                    const title =
-                        prompt(`追加したエルダーの職種は何でしょうか？`);
-
-                    setPerson((person) => ({
-                        ...person,
-                        mentors: [...person.mentors, { name, title }],
-                    }));
-                }}
-            >
-                エルダー追加
-            </button>
-            <button
-                onClick={() => {
-                    const name =
-                        prompt(`削除したいエルダーの名前は何でしょうか？`);
-
-                    setPerson((person) => ({
-                        ...person,
-                        mentors: person.mentors.filter((m) => m.name !== name),
-                    }));
-                }}
-            >
-                エルダー削除
-            </button>
+            <button onClick={handleUpdate}>エルダーの名前を変える</button>
+            <button onClick={handleAdd}>エルダー追加</button>
+            <button onClick={handledelete}>エルダー削除</button>
         </div>
     );
 };
 
 export default AppMentors;
+
+const initialPerson = {
+    name: "siyeon",
+    title: "developer",
+    mentors: [
+        {
+            name: "john",
+            title: "senior developer",
+        },
+        {
+            name: "bob",
+            title: "senior developer",
+        },
+    ],
+};
