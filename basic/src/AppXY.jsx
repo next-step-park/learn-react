999158c18e44c607c86d69e8145764e01ec259eb
next-step-park <next.step.park@gmail.com>
2022-12-09 20:34:21 +0900

    Mouse Pointer Refectoring

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -4,19 +4,17 @@ import "./AppXY.css";
 export default function AppXY() {
     const [mousePos, setMousePos] = useState({});
 
-    useEffect(() => {
-        const handleMouseMove = ({ clientX, clientY }) => {
-            setMousePos({ clientX, clientY });
-        };
-        window.addEventListener("mousemove", handleMouseMove);
-    }, []);
-
     return (
-        <div className="container">
+        <div
+            className="container"
+            onPointerMove={(e) => {
+                setMousePos({ x: e.clientX, y: e.clientY });
+            }}
+        >
             <div
                 className="pointer"
                 style={{
-                    transform: `translate(${mousePos.clientX}px, ${mousePos.clientY}px)`,
+                    transform: `translate(${mousePos.x}px, ${mousePos.y}px)`,
                 }}
             />
             <div>
