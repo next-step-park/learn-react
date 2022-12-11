84712e18d5fae42ec7814f2ccd2c546242255350
SIYEON <next.siyeon@SIYEONs-MacBook-Pro.local>
2022-12-11 21:10:21 +0900

    uncontrolled componentをreducerを使って実装する

--- basic/src/AppForm.jsx
+++ basic/src/AppForm.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { useState } from "react";
+import { useReducer } from "react";
+import { Reducer } from "react";
+import formReducer from "./reducer/form-reducer";
 
 export default function AppForm() {
-    const [form, setForm] = useState({ name: "", email: "" });
+    const [form, dispatch] = useReducer(formReducer, initialForm);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(form);
     };
 
+    // dispatchは既に対象となるObjectと繋がっているので、ここではmethodの中で使いたい値だけを
+    // 引数として渡せば良い
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setForm({ ...form, [name]: value });
+        dispatch({ type: "updated", e });
     };
 
     return (
@@ -37,3 +40,5 @@ export default function AppForm() {
         </form>
     );
 }
+
+const initialForm = { name: "", email: "" };
