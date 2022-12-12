1f245889aa1a3107b691429d75d0c39098b075b9
next-step-park <next.step.park@gmail.com>
2022-12-12 22:01:58 +0900

    use custom hook

--- basic/src/basic/components/Products.jsx
+++ basic/src/basic/components/Products.jsx
@@ -1,28 +1,9 @@
 import React, { useEffect, useState } from "react";
+import useProducts from "../../hooks/use-products";
 
 export default function Products() {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
-    const [products, setProducts] = useState([]);
     const [checked, setChecked] = useState(false);
-
-    useEffect(() => {
-        setLoading(true);
-        setError(undefined);
-
-        fetch(`data/${checked ? "sale_" : ""}products.json`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(
-                    "🔥　温かい最新のデータがネットワークを通して届きました"
-                );
-                setProducts(data);
-            })
-            .catch((e) => setError("エラーが発生した！"))
-            .finally(() => setLoading(false));
-
-        return () => console.log("🧺 不要なものを削除しております");
-    }, [checked]);
+    const [loading, error, products] = useProducts({ salesOnly: checked });
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>{error}</p>;
